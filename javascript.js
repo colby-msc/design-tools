27e47749dbe76c0e4c27734eab7cd93147db83b0
Colby Richie <237321858+colby-msc@users.noreply.github.com>
2026-07-15 13:23:26 -0500

    Add more cards and update existing cards

--- javascript.js
+++ javascript.js
@@ -10,6 +10,62 @@ function calculateMinimumGrilleArea(requiredAirflow) {
 
 }
 
+function calculateMinimumGrilleDimensions(requiredAirflow) {
+    const maximumFaceVelocity = 400;
+    const assumedOpenGrilleArea = 0.65;
+
+    const minimumFreeArea = ((requiredAirflow / maximumFaceVelocity) * 144); // converting from square feet to square inches
+
+    const minimumGrilleArea = Math.ceil((minimumFreeArea / assumedOpenGrilleArea));
+
+
+    const grilleHeights = [4, 6, 8, 10, 12];
+    let grilleDimensions = [];
+
+    for (let i = 0; i < grilleHeights.length; i++) {
+
+        let grilleLength = (minimumGrilleArea / grilleHeights[i]);
+
+        if (grilleLength >= grilleHeights[i]) {
+
+            if (grilleLength <= 8) {
+                grilleLength = 8;
+            } else if (grilleLength <= 10) {
+                grilleLength = 10;
+            } else if (grilleLength <= 12) {
+                grilleLength = 12;
+            } else if (grilleLength <= 14) {
+                grilleLength = 14;
+            } else if (grilleLength <= 16) {
+                grilleLength = 16;
+            } else if (grilleLength <= 18) {
+                grilleLength = 18;
+            } else if (grilleLength <= 24) {
+                grilleLength = 24;
+            } else if (grilleLength <= 30) {
+                grilleLength = 30;
+            } else if (grilleLength <= 36) {
+                grilleLength = 36;
+            } else if (grilleLength <= 48) {
+                grilleLength = 48;
+            } else if (grilleLength <= 60) {
+                grilleLength = 60;
+            } else {
+                grilleLength = Math.ceil(grilleLength);
+            }
+
+            grilleDimensions.push(` ${grilleLength}/${grilleHeights[i]}`);
+        }
+    }
+
+    if (grilleDimensions.length == 0) {
+        grilleDimensions.push("8/4");
+    }
+
+    return grilleDimensions;
+
+}
+
 function calculateMinimumVentilation(conditionedFloorArea, occupants) {
     const floorAreaVentilationFactor = 0.03;
     const occupantVentilationRate = 7.5;
@@ -19,7 +75,7 @@ function calculateMinimumVentilation(conditionedFloorArea, occupants) {
     return `${Math.ceil(requiredMinimumContinuousVentilationAirflow)} CFM`;
 }
 
-function calculateDuctSize(requiredAirflow) {
+function calculateRoundDuctSize(requiredAirflow) {
     let roundDuctDiameter;
 
     if (requiredAirflow <= 15) {
@@ -46,23 +102,62 @@ function calculateDuctSize(requiredAirflow) {
         roundDuctDiameter = 18;
     } else if (requiredAirflow <= 1700) {
         roundDuctDiameter = 20;
+    } else if (requiredAirflow <= 2300) {
+        roundDuctDiameter = 22;
     } else {
-        return "CFM Currently Not Calculable";
+        return "Enter ≤ 2300 CFM";
     }
 
     return `${roundDuctDiameter}"`;
 
 }
 
+function calculateRectangularDuctSize(requiredAirflow) {
+    let rectangularDuctDiameter;
+
+    if (requiredAirflow <= 15) {
+        rectangularDuctDiameter = "4/4, 5/3";
+    } else if (requiredAirflow <= 46) {
+        rectangularDuctDiameter = "5/5, 6/4, 8/3";
+    } else if (requiredAirflow <= 75) {
+        rectangularDuctDiameter = "6/6, 7/5, 8/4";
+    } else if (requiredAirflow <= 110) {
+        rectangularDuctDiameter = "7/6, 9/5, 12/4";
+    } else if (requiredAirflow <= 160) {
+        rectangularDuctDiameter = "8/7, 10/6, 12/5";
+    } else if (requiredAirflow <= 220) {
+        rectangularDuctDiameter = "9/8, 10/7, 12/6";
+    } else if (requiredAirflow <= 290) {
+        rectangularDuctDiameter = "10/9, 12/8, 14/7";
+    } else if (requiredAirflow <= 460) {
+        rectangularDuctDiameter = "12/12, 14/9, 16/8";
+    } else if (requiredAirflow <= 700) {
+        rectangularDuctDiameter = "14/12, 18/10, 20/9";
+    } else if (requiredAirflow <= 1000) {
+        rectangularDuctDiameter = "16/14, 25/10, 18/12";
+    } else if (requiredAirflow <= 1300) {
+        rectangularDuctDiameter = "18/16, 20/14, 25/12";
+    } else if (requiredAirflow <= 1700) {
+        rectangularDuctDiameter = "20/18, 25/14, 30/12";
+    } else if (requiredAirflow <= 2300) {
+        rectangularDuctDiameter = "25/18, 30/14, 40/12";
+    }
+
+    return rectangularDuctDiameter;
+
+}
+
 const airflowOutput = document.querySelector("#airflow");
 const grilleButton = document.querySelector(".grille-button");
 const grilleArea = document.querySelector(".grille-area");
+const grilleDimensions = document.querySelector(".grille-dimensions");
 
 grilleButton.addEventListener("click", () => {
 
     const selection = airflowOutput.value;
 
     grilleArea.textContent = calculateMinimumGrilleArea(selection);
+    grilleDimensions.textContent = calculateMinimumGrilleDimensions(selection);
 
     event.preventDefault();
 
@@ -85,13 +180,17 @@ ventilationButton.addEventListener("click", () => {
 
 const ductAirflowOutput = document.querySelector("#duct-airflow");
 const ductButton = document.querySelector(".duct-button");
-const ductSize = document.querySelector(".duct-size");
+const roundDuctSize = document.querySelector(".round-duct-size");
+const rectangularDuctSize = document.querySelector(".rectangular-duct-size");
+
 
 ductButton.addEventListener("click", () => {
 
     const selection = ductAirflowOutput.value;
 
-    ductSize.textContent = calculateDuctSize(selection);
+    roundDuctSize.textContent = calculateRoundDuctSize(selection);
+    rectangularDuctSize.textContent = calculateRectangularDuctSize(selection);
+
 
     event.preventDefault();
 });
